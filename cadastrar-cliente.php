5e5eb3dccf05cc3200f3e7aca93d3525567a1905
carllos39 <carllos2005@outlook.com.br>
2023-10-31 22:27:59 -0300

    correção

--- cadastrar-cliente.php
+++ cadastrar-cliente.php
@@ -6,12 +6,7 @@ include("lavanderia-controle.php");
 <?php 
 $nome=$_POST['nome'];
 $email=$_POST['email'];
-$dataNascimento=$_POST['dataNascimento'];
-$result=explode('/',$dataNascimento);
-$dia=result[0];
-$mes=result[1];
-$ano=result[2];
-$dataNascimento=[$ano.'-'.$mes.'-'.$dia ] ;
+$nascimento=$_POST['nascimento'];
 $cep=$_POST['cep'];
 $endereco=$_POST['endereco'];
 $bairro=$_POST['bairro'];
@@ -20,7 +15,7 @@ $estado=$_POST['estado'];
 $telefone=$_POST['telefone'];
 $assunto=$_POST['assunto'];
 $mensagem=$_POST['mensagem'];
-if(inserirCliente($conexao,$nome,$email,$dataNascimento,$cep,$endereco,$bairro,$cidade,$estado,$telefone,$assunto,$mensagem)){
+if(inserirCliente($conexao,$nome,$email,$nascimento,$cep,$endereco,$bairro,$cidade,$estado,$telefone,$assunto,$mensagem)){
     echo"Cadastrado com sucesso!";
 }else{
     echo"Não cadastrou!";
